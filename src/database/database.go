292f806dd5ad9aeb5dda842ca113360e8e999f4b
Masoud Banimahd <masoud.banimahd@polestar.com>
2024-05-02 17:41:02 +0200

    implement database module

---
+++ src/database/database.go
@@ -0,0 +1,83 @@
+package database
+
+import (
+	"net/mail"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/dynamodb"
+	"github.com/dev3mike/go-serverless-boilerplate/src/types"
+)
+
+const (
+	TABLE_NAME = "users"
+)
+
+type DynamoDbClient struct {
+	dbClient *dynamodb.DynamoDB
+}
+
+
+func NewDynamoDbClient() DynamoDbClient {
+
+	dbSession := session.Must(session.NewSession())
+	dbClient := dynamodb.New(dbSession)
+
+ return DynamoDbClient{
+	dbClient: dbClient,
+ }
+}
+
+func(db DynamoDbClient) DoesUserExist(email string) (bool, error){
+
+	validatedEmail, err := mail.ParseAddress(email);
+
+	if err != nil {
+		return true, err;
+	}
+
+	result, err := db.dbClient.GetItem(&dynamodb.GetItemInput{
+		TableName: aws.String(TABLE_NAME),
+		Key: map[string]*dynamodb.AttributeValue{
+			"email": {
+				S: aws.String(validatedEmail.Address),
+			},
+		},
+	})
+
+	if err != nil{
+		return true, err
+	}
+
+	if result.Item != nil {
+		return false, nil
+	}
+
+	return true, nil;
+}
+
+func(db DynamoDbClient) CreateUser(userDto types.UserDto) error {
+	item := &dynamodb.PutItemInput{
+		TableName: aws.String(TABLE_NAME),
+		Item: map[string]*dynamodb.AttributeValue{
+			"email": {
+				S: aws.String(userDto.Email),
+			},
+			"firstName": {
+				S: aws.String(userDto.FirstName),
+			},
+			"lastName": {
+				S: aws.String(userDto.LastName),
+			},
+			"passwordHash": {
+				S: aws.String(userDto.Password),
+			},
+		},
+	}
+
+	_, err := db.dbClient.PutItem(item)
+
+	if err != nil{
+		return err
+	}
+}
