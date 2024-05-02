051871c9ae9e586952e4b4d46935733f8742b68c
Masoud Banimahd <masoud.banimahd@polestar.com>
2024-05-02 19:04:12 +0200

    add validator helper

---
+++ src/helpers/validators.go
@@ -0,0 +1,20 @@
+package helpers
+
+import (
+	"fmt"
+	"strings"
+)
+
+type Input struct{
+	Name string
+	Value string
+}
+
+func ValidateInput(inputs ...Input) error{
+	for _, field := range inputs {
+        if strings.TrimSpace(field.Value) == "" {
+            return fmt.Errorf("%s could not be empty", field.Name)
+        }
+    }
+    return nil
+}
